46751d6aa4429be90947ba7eae27c7f1765c33ac
Anthony <anthony.cattet@gmail.com>
2022-05-12 09:46:57 -0400

    parties

--- server.js
+++ server.js
@@ -85,7 +85,8 @@ app.post('/api/candidate', ({ body }, res) => {
       res.status(400).json({ error: errors });
       return;
     }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+
+const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
   VALUES (?,?,?)`;
 const params = [body.first_name, body.last_name, body.industry_connected];
 
